07e5abf8613464b6d9fa68a45f5df693422d2f22
TheWinterGeneral <ArloJ09@hotmail.com>
2024-04-27 17:24:32 -0700

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 itemPrice = input("Item price: ")
-salesTax = float(.056)
+# Change the float to your sales tax percent divided by 100
+salesTax = float(Your Sales Tax)
 taxammount = int(itemPrice) * salesTax
 totalAmount = float(itemPrice) + float(taxammount)
 print(totalAmount)
